4ff20a2ff09eb4ba511071db6872e9d9332e144f
aoikurokawa <ht.so.ik0102@gmail.com>
2025-02-09 20:26:34 +0900

    feat: update

--- src/restaking/index.ts
+++ src/restaking/index.ts
@@ -4,6 +4,10 @@ export * from "./errors";
 export * from "./instructions";
 export * from "./types";
 
+const CONFIG_SEED = "config";
+const OPERATOR_SEED = "operator";
+const NCN_SEED = "ncn";
+
 /**
  * Program address
  *
@@ -19,3 +23,48 @@ export const PROGRAM_ADDRESS = "RestkWeAVL8fRGgzhfeoqFhsqKRchg6aa1XrcH96z4Q";
  * @category generated
  */
 export const PROGRAM_ID = new PublicKey(PROGRAM_ADDRESS);
+
+/**
+ * Restaking Config public key
+ */
+export const findConfigPDA = () => {
+  const seedBuffer = Buffer.from(CONFIG_SEED, "utf8");
+
+  const [pda, bump] = PublicKey.findProgramAddressSync(
+    [seedBuffer],
+    PROGRAM_ID,
+  );
+
+  return { pda, bump, seeds: [seedBuffer] };
+};
+
+/**
+ * Restaking Operator public key
+ */
+export const findOperatorPDA = (base: PublicKey) => {
+  const seedBuffer = Buffer.from(OPERATOR_SEED, "utf8");
+  const baseBuffer = base.toBuffer();
+
+  const [pda, bump] = PublicKey.findProgramAddressSync(
+    [seedBuffer, baseBuffer],
+    PROGRAM_ID,
+  );
+
+  return { pda, bump, seeds: [seedBuffer] };
+};
+
+/**
+ * Restaking NCN public key
+ */
+export const findNcnPDA = (base: PublicKey) => {
+  const seedBuffer = Buffer.from(NCN_SEED, "utf8");
+  const baseBuffer = base.toBuffer();
+
+  const [pda, bump] = PublicKey.findProgramAddressSync(
+    [seedBuffer, baseBuffer],
+    PROGRAM_ID,
+  );
+
+  return { pda, bump, seeds: [seedBuffer] };
+};
+
