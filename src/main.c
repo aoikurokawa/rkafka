9c92cf8b0265108fef8d18af1af8f38d33294342
Aoi Kurokawa <62386689+aoikurokawa@users.noreply.github.com>
2025-10-13 11:29:58 +0900

    Function (#12)

--- src/main.c
+++ src/main.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
+void does_not_exist(char name[]) {
+	printf("%s\n", name);
+}
+
 int main() {
-	printf("Starting the Python interpreter...\n");
-	return 0;
+	printf("starting sneklang tools\n");
+	does_not_exist("uh oh");
+	printf("finished sneklang tools\n");
 }
