4ab9c0efb86bedb0dace1f8a957acc1382a143bd
Aoi Kurokawa <62386689+aoikurokawa@users.noreply.github.com>
2025-10-13 15:42:57 +0900

    Correlation (#18)

--- src/main.rs
+++ src/main.rs
@@ -3,30 +3,57 @@ use std::{
     net::TcpListener,
 };
 
-struct Message {
-    // message_size: u32,
+#[derive(Debug)]
+struct RequestHeaderV2 {
+    /// Request API key
     request_api_key: i16,
+
+    /// Request API version
     request_api_version: i16,
+
+    /// Correlation ID
     correlation_id: i32,
-    client_id: Option<String>,
-    tag_buffer: Vec<u8>,
 }
 
-impl Message {
-    fn new(
-        //   message_size: u32,
-        request_api_key: i16,
-        request_api_version: i16,
-        correlation_id: i32,
-    ) -> Self {
-        Self {
-            //    message_size,
-            request_api_key,
-            request_api_version,
-            correlation_id,
-            client_id: None,
-            tag_buffer: vec![],
+impl TryFrom<&[u8]> for RequestHeaderV2 {
+    type Error = String;
+
+    fn try_from(value: &[u8]) -> Result<Self, Self::Error> {
+        if value.len() < 2 + 2 + 4 {
+            return Err("Size constraints not satisfied".to_string());
         }
+
+        Ok(Self {
+            request_api_key: i16::from_be_bytes(value[0..2].try_into().unwrap()),
+            request_api_version: i16::from_be_bytes(value[2..4].try_into().unwrap()),
+            correlation_id: i32::from_be_bytes(value[4..8].try_into().unwrap()),
+        })
+    }
+}
+
+#[derive(Debug)]
+struct RequestMessage {
+    message_size: i32,
+    header: RequestHeaderV2,
+    // body
+}
+
+impl TryFrom<&[u8]> for RequestMessage {
+    type Error = String;
+
+    fn try_from(value: &[u8]) -> Result<Self, Self::Error> {
+        if value.len() < 4 + 2 + 2 + 4 {
+            return Err("Size constraints not satisfied".to_string());
+        }
+        let message_size = i32::from_be_bytes(value[..4].try_into().unwrap());
+        let header = RequestHeaderV2::try_from(&value[4..12]).unwrap();
+        // let body = String::from_utf8(value[12..].to_vec()).unwrap();
+
+        Ok(Self {
+            message_size,
+            header,
+            //   body,
+        })
     }
 }
 
@@ -41,12 +68,12 @@ fn main() {
         match stream {
             Ok(mut stream) => {
                 let mut size_buf = [0; 4];
+                // let n = stream.read(buf)
                 if let Err(e) = stream.read_exact(&mut size_buf) {
                     println!("Error reading message size: {e}");
                     continue;
                 }
                 let message_size = u32::from_be_bytes(size_buf);
-                println!("Message size: {message_size}");
 
                 let mut message_buf = vec![0; message_size as usize];
                 if let Err(e) = stream.read_exact(&mut message_buf) {
@@ -63,10 +90,12 @@ fn main() {
                     message_buf[7],
                 ]);
 
-                println!("Correlation ID: {correlation_id}");
-
                 stream
                     .write_all(&mut [
+                        0,
+                        0,
+                        0,
+                        0,
                         message_buf[4],
                         message_buf[5],
                         message_buf[6],
