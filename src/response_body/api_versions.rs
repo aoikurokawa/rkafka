5f6639b3b7e867b3e38b5b5f8cb854240e368ccd
Aoi Kurokawa <62386689+aoikurokawa@users.noreply.github.com>
2025-10-19 06:45:50 +0900

    Add size (#28)

--- src/response_body/api_versions.rs
+++ src/response_body/api_versions.rs
@@ -1,6 +1,6 @@
 use crate::{api_key::ApiKey, body::Body, error_code::ErrorCode};
 
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy)]
 pub struct ApiSupport {
     /// The API index
     pub api_key: ApiKey,
@@ -28,12 +28,36 @@ impl Body for ApiVersionsResponse {
         let error_code = self.error_code as i16;
         bytes.extend_from_slice(&error_code.to_be_bytes());
 
+        for api in self.api_keys.iter() {
+            let api_key = api.api_key as i16;
+            bytes.extend_from_slice(&api_key.to_be_bytes());
+
+            bytes.extend_from_slice(&api.min_version.to_be_bytes());
+            bytes.extend_from_slice(&api.max_version.to_be_bytes());
+        }
+
         bytes
     }
 
     fn size(&self) -> u32 {
         let error_code = self.error_code as i16;
-        error_code.to_be_bytes().len() as u32
+        let error_code_len = error_code.to_be_bytes().len() as u32;
+
+        let api_keys_len: u32 = self
+            .api_keys
+            .iter()
+            .map(|key| {
+                let api_key = key.api_key as i16;
+                let api_key_len = api_key.to_be_bytes().len() as u32;
+
+                let min_version_len = key.min_version.to_be_bytes().len() as u32;
+                let max_version_len = key.max_version.to_be_bytes().len() as u32;
+
+                api_key_len + min_version_len + max_version_len
+            })
+            .sum();
+
+        error_code_len + api_keys_len
     }
 }
 
